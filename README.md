488b87939a0cd25e5a4e0a69a825b96c9c933e85
Tejinder Singh Bedi <bedisahib95@gmail.com>
2020-08-06 12:42:39 +0200

    Repo initialized with stuff

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# gfg-questions
+# GFG Questions and their Soutions
+<H2>Problem number | Problem Descritption | Program Name</H4>
+
+<h4>Problem 1</h4>
+<p>
+Given a string, find the smallest window length with all distinct characters of the given string. For eg. str = “aabcbcdbca”, then the result would be 4 as of the smallest window will be “dbca” .
+</p> 
+<h5>SmallestSubstringWindowContainingAllCharacters</h5>
+
---
+++ src/main/java/com/gfg/questions/SmallestSubstringWindowContainingAllCharacters.java
@@ -0,0 +1,16 @@
+package com.gfg.questions;
+
+public class SmallestSubstringWindowContainingAllCharacters {
+
+    private static final String INPUT = "";
+    private static final int OUTPUT = 0;
+
+    public static void main(String[] args) {
+
+        assert getSmallestSubstringWindowLength(INPUT) == OUTPUT;
+    }
+
+    private static int getSmallestSubstringWindowLength(String input) {
+        return 0;
+    }
+}
